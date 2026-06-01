ab9da3ba0859ac36da78a21f719b388ae75af79f
RiyaThakor024 <riya.thakor024@gmail.com>
2026-06-01 16:40:47 +0530

    test

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,2 @@
-function Welcome(name: string) {
-    const user = {
-        name: name,
-    };
-
-    const u1 = user.name;
-    return u1;
-
-}
-Welcome('india');
+import { Config } from './config/index.js';
+console.log(Config.PORT);
