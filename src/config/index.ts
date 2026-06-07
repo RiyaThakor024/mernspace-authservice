fcffcc7d93da90c7f70d534917763590e23d058a
RiyaThakor024 <riya.thakor024@gmail.com>
2026-06-08 00:06:16 +0530

    setup datavase

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,18 @@
 import { config } from 'dotenv';
+import path from 'path';
 
-config();
+config({
+    path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`),
+});
+
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
-const { NODE_ENV } = process.env;
 
 export const Config = {
     PORT: port,
-    NODE_ENV,
+    NODE_ENV: process.env.NODE_ENV!,
+    DB_HOST: process.env.DB_HOST!,
+    DB_PORT: Number(process.env.DB_PORT!),
+    DB_USERNAME: process.env.DB_USERNAME!,
+    DB_PASSWORD: process.env.DB_PASSWORD!,
+    DB_NAME: process.env.DB_NAME!,
 };
