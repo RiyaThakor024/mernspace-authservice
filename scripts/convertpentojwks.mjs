7ab8cbbc785e9e3308252069d49220ad73028afd
RiyaThakor024 <riya.thakor024@gmail.com>
2026-06-19 18:53:50 +0530

    setup database

--- scripts/convertpentojwks.mjs
+++ scripts/convertpentojwks.mjs
@@ -3,5 +3,5 @@ import rsaPemToJwk from 'rsa-pem-to-jwk';
 import { json } from 'stream/consumers';
 const privateKey = fs.readFileSync('./certs/private.pem');
 
-const jwk = rsaPemToJwk(privateKey,{use:'sig'},'public');
+const jwk = rsaPemToJwk(privateKey, { use: 'sig' }, 'public');
 console.log(JSON.stringify(jwk));
