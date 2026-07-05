a82810a1a8d63fea7a26bcb7c0be56fdd270a6ce
RiyaThakor024 <riya.thakor024@gmail.com>
2026-07-05 15:31:43 +0530

    fix: Debugging test case pipeline

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -9,17 +9,12 @@ export const AppDataSource = new DataSource({
     password: Config.DB_PASSWORD,
     database: Config.DB_NAME,
     //don't use in production
+    ssl: {
+        rejectUnauthorized: false,
+    },
     synchronize: Config.NODE_ENV === 'test' || Config.NODE_ENV === 'dev',
     logging: false,
     entities: ['src/entities/*.{ts,js}'],
     migrations: ['src/migration/*.{ts,js}'],
     subscribers: [],
-    extra:
-        Config.NODE_ENV === 'test'
-            ? {}
-            : {
-                  ssl: {
-                      rejectUnauthorized: false,
-                  },
-              },
 });
