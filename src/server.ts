359af600201729265ffc150be6c10ab736510bdc
RiyaThakor024 <riya.thakor024@gmail.com>
2026-07-08 18:27:56 +0530

    add file

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,13 @@ import { Config } from './config/index';
 import app from './app';
 import { AppDataSource } from './config/data-source';
 import { logger } from './config/logger';
+import { createAdmin } from './utils';
 
 const startServer = async () => {
     const PORT = Config.PORT;
     try {
         await AppDataSource.initialize();
+        await createAdmin();
         logger.info('database conected successfuly');
         app.listen(PORT, () => {
             console.log(`Listening on port ${PORT}`);
